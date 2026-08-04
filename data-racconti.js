779614ff2e2896d9e574131f5981c63480c014a7
Sakrem <sakrem@users.noreply.github.com>
2026-08-04 10:12:39 +0000

    Il Chiamato: banner 70px a sinistra (bannerOffset -45->25)

--- data-racconti.js
+++ data-racconti.js
@@ -728,7 +728,7 @@ const LIBRI = {
     filigrana: "https://pub-de8310383cdb437f8f0b585a6642e88e.r2.dev/Il%20Chiamato%20Filigrana.jpg",
     filigranaLeftFade: true,
     filigranaBottomFade: true,
-    bannerOffset: -45,
+    bannerOffset: 25,
     title: {
       it: "Il Chiamato",
       en: "The Called"
